09c6611219de21a0dd643a4158e0a5999bebf0e7
Vladimir Pezzole <32854289+vladimirpezzole@users.noreply.github.com>
2019-07-01 12:49:54 -0300

    Revert "feat: start text robot"

--- index.js
+++ index.js
@@ -1,17 +1,10 @@
 const readline = require('readline-sync')
-const robots = {
-	//userInput: require('./robots/user-input.js')
-	text: require('./robots/text.js')
-}
 
-async function start() {
+function start() {
 	const content = {}
 
 	content.searchTerm = askAndReturnSearchTerm()
 	content.prefix = askAndReturnPrefix()
-
-	//robots.userInput(content)
-	await robots.text(content)
 
 	function askAndReturnSearchTerm() {
 		return readline.question('Escreva o termo wikipedia a ser procurado: ')
